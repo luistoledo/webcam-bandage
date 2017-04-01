563cb2659f5b926ec41a787580ea985c937c11f4
Luis Toledo <luisernesto.toledo@gmail.com>
2017-04-01 20:56:55 +0200

    Initial Commit

---
+++ webcam bandage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  webcam bandage
+//
+//  Created by Luis on 4/1/17.
+//  Copyright © 2017 Luis Toledo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ webcam bandage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  webcam bandage
+//
+//  Created by Luis on 4/1/17.
+//  Copyright © 2017 Luis Toledo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
